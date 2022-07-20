a504617b8432f21957cfa7c41b458e2b6e8f7f41
Ritvik Niraula <saniritvik@gmail.com>
2022-07-20 15:44:09 -0500

    added shopping cart files

---
+++ shoppingCart/addCart.py
@@ -0,0 +1,60 @@
+from operator import delitem
+
+cart=[]
+def addCart():
+    numberOfItems=int(input("How many items do you want to add to cart?"))
+    for i in range(numberOfItems):
+        cartItem=input("What item do you want to add to cart?")
+        quantity=int(input("How many of " + cartItem + " do you want to buy?"))
+        cart.append(cartItem + "||" + str(quantity))
+
+def delEntireCart(fileName):
+    yn=input("User are you sure you want to delete the enitire cart? (y/n)")
+    if yn == "y":
+        print("Deleting cart")
+        filePointer=open(fileName,mode="w")
+        cart.clear()
+    elif yn == "n":
+        print("Okay")
+    else:
+        print("That is not a valid answer")
+        delEntireCart()
+
+def viewCart():
+    totalOfItems=0
+    print("Items found")
+    print("Item name -- quanity")
+    for item in cart:
+        print(item.split("||")[0] + "- - -" + (item.split("||"))[1])
+        totalOfItems+= int(item.split("||")[1])
+    print("Number of items --" + str(totalOfItems))
+
+def deleteItem():
+    viewCart()
+    delItem=input("What is the item you want to delete?")
+    cart.remove(delItem)
+
+def saveItems(fileName):
+    filePointer=open(fileName,mode="w")
+    if len(cart) > 0:
+        for item in cart:
+            filePointer.writelines(item + "\n")
+    filePointer.close()
+
+def loadCart(fileName):
+    cart.clear()
+    filePointer=open(fileName,mode="r")
+    fileData=filePointer.readlines()
+    for line in fileData:
+        cart.append(line.strip())
+    filePointer.close()
+
+def delScreen():
+    print(chr(27) + "[2J")
+
+def displayScreen(screenName):
+    print("*********************************")
+    print("*********************************")
+    print("**********" + screenName + "************")
+    print("*********************************")
+    print("*********************************")
---
+++ shoppingCart/items.py
@@ -0,0 +1,58 @@
+from functools import total_ordering
+
+
+itemsFileName="items.txt"
+totalItems=[]
+itemsPrice=[]
+def itemsMenu():
+    stop = False
+    while not stop:
+        itemsMenyEntry=int(input("What do you want to do? \n 1 View items \n 2 Add new item \n 3 Delete item \n 4 exit to main menu \n"))
+        if itemsMenyEntry == 1:
+            viewItems()
+        elif itemsMenyEntry == 2:
+            addNewItem()
+        elif itemsMenyEntry == 3:
+            print("No.")
+        elif itemsMenyEntry == 4:
+            stop=True
+        else: 
+            print("NOT AN OPTION READ BETTER")
+
+def viewItems():
+    filePointer=open(itemsFileName,mode="r")
+    itemsRead=filePointer.readlines()
+    print("Here are your availble items")
+    print("Index --- item name -- price")
+    index=1
+    for lines in itemsRead:
+        print(str(index) + "." + lines.split("||")[0] + "---" + str(lines.split("||")[1]),end=" ")
+        totalItems.append(lines.split("||")[0])
+        itemsPrice.append(lines.split("||")[1])
+        index+= 1
+    print("")
+    item=int(input("Choose the index to add item to cart"))
+    print(totalItems[item-1])
+    print(itemsPrice[item-1])
+
+def addNewItem():
+    filePointer=open(itemsFileName,mode="a+")
+    readLines=filePointer.readlines()
+    print(readLines)
+    existItem=[]
+    for lines in readLines:
+        existItem.append(lines.split("||")[0].lower())
+    item=input("What do you want to add?")
+    lowerd=item.lower()
+    price=float(input("How much does it cost?"))
+    print(existItem)
+    print(lowerd)
+    if lowerd in existItem:
+        print("ADMIN ITS ALREADY THERE READ BRO, TRY AGAIN")
+    else:
+        filePointer.writelines("\n" + item + "||" + str(price))
+        print("Item added")
+
+
+        
+
