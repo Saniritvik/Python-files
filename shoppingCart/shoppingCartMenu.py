f967c238ffb735b5fa1e4c999b39f5be0ce60020
hari <hari.deepak1@gmail.com>
2022-07-20 15:57:31 -0500

    updated

--- shoppingCart/shoppingCartMenu.py
+++ shoppingCart/shoppingCartMenu.py
@@ -1,22 +1,24 @@
 from addCart import *
 from items import *
-mainScreenNames={
-    1:"Ritvik",
-    2:"Neeta",
-    3:"Rishi"
+mainScreenNames = {
+    1: "Ritvik",
+    2: "Neeta",
+    3: "Rishi"
 }
-    
-fileNames={
-    1:"Ritvik.txt",
-    2:"Neeta.txt",
-    3:"Rishi.txt"
+
+fileNames = {
+    1: "Ritvik.txt",
+    2: "Neeta.txt",
+    3: "Rishi.txt"
 }
+
 
 def shoppingCartScreen(entry):
     stop = False
     while not stop:
         displayScreen("Home Screen")
-        shoppingCartEntry=int(input("What do you want to do? \n 1 View cart \n 2 Delete something \n 3 Add something to cart \n 4 Save and exit to main menu \n 5 Delete THE ENTIRE CART"))
+        shoppingCartEntry = int(input(
+            "What do you want to do? \n 1 View cart \n 2 Delete something \n 3 Add something to cart \n 4 Save and exit to main menu \n 5 Delete THE ENTIRE CART"))
         if shoppingCartEntry == 1:
             delScreen()
             viewCart()
@@ -29,19 +31,21 @@ def shoppingCartScreen(entry):
         elif shoppingCartEntry == 4:
             delScreen()
             saveItems(fileNames[entry])
-            stop=True
+            stop = True
         elif shoppingCartEntry == 5:
             delScreen()
             delEntireCart(fileNames[entry])
 
-        else: 
+        else:
             print("NOT AN OPTION READ BETTER")
+
 
 def mainScreen():
     while True:
         displayScreen("User Screen")
-        entry=int(input("WHO ARE YOU?? \n 1 Ritvik \n 2 Neeta \n 3 Rishi \n 4 EXIT \n 5 Admin"))
-        menuNames=mainScreenNames.keys()
+        entry = int(
+            input("WHO ARE YOU?? \n 1 Ritvik \n 2 Neeta \n 3 Rishi \n 4 EXIT \n 5 Admin"))
+        menuNames = mainScreenNames.keys()
         if entry in menuNames:
             delScreen()
             print("Hello " + mainScreenNames[entry])
@@ -57,5 +61,6 @@ def mainScreen():
             delScreen()
             print("That person DOES NOT EXIST please try again")
             mainScreen()
+
 
 mainScreen()
