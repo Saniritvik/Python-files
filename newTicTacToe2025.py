220cf9b06f7f8f8ffc953d088daf2badbeae83cc
The Spaghetti Coder <100983825+Saniritvik@users.noreply.github.com>
2025-12-02 17:13:53 +0000

    Implemented basic game loop and idae

--- newTicTacToe2025.py
+++ newTicTacToe2025.py
@@ -14,18 +14,21 @@ def tic_tac_toe():
         9: "#"
     }
     filled = []
+        
+    print("Numbers on grid")
+    print("1|2|3")
+    print("4|5|6")
+    print("7|8|9")
     
-    while True: 
-        print("Numbers on grid")
-        print("1|2|3")
-        print("4|5|6")
-        print("7|8|9")
+    print("Current grid")
+    print(grid_dict[1] + "|" + grid_dict[2] + "|" + grid_dict[3])
+    print(grid_dict[4] + "|" + grid_dict[5] + "|" + grid_dict[6])
+    print(grid_dict[7] + "|" + grid_dict[8] + "|" + grid_dict[9])
         
-        print("Current grid")
-        print(grid_dict[1] + "|" + grid_dict[2] + "|" + grid_dict[3])
-        print(grid_dict[4] + "|" + grid_dict[5] + "|" + grid_dict[6])
-        print(grid_dict[7] + "|" + grid_dict[8] + "|" + grid_dict[9])
-        
-        
+    for i in range(1,10): 
+        turn_player = input("Place your marker on the board(1-9)")
+        filled = True
+
+
 
 tic_tac_toe()
